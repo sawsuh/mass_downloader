08fb491053883b74bac0e1b5154a9c47bd9885f4
Prashant Rao <prao060@gmail.com>
2024-01-07 21:53:43 +1100

    fixed bug in main

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ from scdownloader import SCDownloader
 if __name__ == '__main__':
     with open('urls.txt') as urls:
         downloader = SCDownloader(rm_dls = False)
-        downloader.load_urls(
-            [url for url in urls.read().split('\n') if len(url)>0]
-        )
+        downloader.load_urls(urls.read())
         downloader.execute()
